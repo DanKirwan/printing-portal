cc8785512c9afa593d0a41040f53bee7c8046b16
Dan Kirwan <dan.kirwan20@imperial.ac.uk>
2022-12-30 14:00:54 +0000

    Aesthetic changes to OrderComplete page

--- src/pages/OrderComplete.tsx
+++ src/pages/OrderComplete.tsx
@@ -4,12 +4,18 @@ import { useNavigate } from "react-router-dom";
 export default () => {
     const navigate = useNavigate();
     return (
-        <Stack>
-            <Container>
+        <Stack height='100%' width='100%' alignItems='center' justifyContent='center'>
+            <Container maxWidth='sm'>
+                <Stack spacing={2}>
 
-                <Typography>Order Complete!</Typography>
-                <Button onClick={() => navigate('/')} variant='contained'>Start Another Order</Button>
-                {/* <Button onClick={() => navigate('/orders')} variant='contained'>Start Another Order</Button> */}
+
+                    <Typography variant='h4' textAlign='center'>Order Complete!</Typography>
+                    <Typography variant='caption' textAlign='center'>
+                        You will receieve a confirmation email once your order is confirmed.
+                    </Typography>
+                    <Button onClick={() => navigate('/')} variant='contained'>Start Another Order</Button>
+                    {/* <Button onClick={() => navigate('/orders')} variant='contained'>Start Another Order</Button> */}
+                </Stack>
             </Container>
         </Stack>
     )
