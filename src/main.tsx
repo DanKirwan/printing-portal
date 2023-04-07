b910ca6cada9a5596744dc456aaa3df81a7bb27a
Dan Kirwan <dan.kirwan20@imperial.ac.uk>
2023-04-07 01:31:00 +0100

    Added consent to google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFirestoreEmulator, enableIndexedDbPersistence, getFirestore } from "firebase/firestore";
 import { getStorage, ref, getDownloadURL, connectStorageEmulator } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, setConsent } from "firebase/analytics";
+import { useCookieConsent } from '@use-cookie-consent/core';
+
+const { consent } = useCookieConsent();
+const toConsent = (consented?: boolean) => consented ? 'granted' : 'denied';
 
 
 const firebaseConfig = {
@@ -23,8 +27,15 @@ export const storage = getStorage(app);
 export const auth = getAuth(app);
 export const fireStore = getFirestore(app);
 export const analytics = getAnalytics(app);
+
+
+setConsent({
+  ad_storage: 'denied',
+  analytics_storage: toConsent(consent.statistics),
+  functionality_storage: toConsent(consent.necessary)
+});
+
 if (import.meta.env.VITE_USE_EMULATORS) {
-  console.log("Test");
   connectFirestoreEmulator(fireStore, 'localhost', 5050);
   // connectAuthEmulator(auth, 'localhost:9099');
   connectStorageEmulator(storage, 'localhost', 9199);
